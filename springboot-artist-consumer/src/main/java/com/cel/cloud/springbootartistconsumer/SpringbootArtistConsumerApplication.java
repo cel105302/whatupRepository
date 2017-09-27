572656eab843fd8bebc0b4ce6837304744a72a80
Carl-Che <Carl-Che@169.254.217.60>
2017-09-27 17:32:46 +0800

    first commit;

---
+++ springboot-artist-consumer/src/main/java/com/cel/cloud/springbootartistconsumer/SpringbootArtistConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.cel.cloud.springbootartistconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArtistConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootArtistConsumerApplication.class, args);
+	}
+}
