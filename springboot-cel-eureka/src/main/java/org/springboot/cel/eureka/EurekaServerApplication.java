e6abae52a511cf4f332e271da5f4d998548a1ad0
Carl-Che <Carl-Che@192.168.2.207>
2017-09-29 17:03:06 +0800

    hj

---
+++ springboot-cel-eureka/src/main/java/org/springboot/cel/eureka/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.cel.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class,args);
+	}
+}
